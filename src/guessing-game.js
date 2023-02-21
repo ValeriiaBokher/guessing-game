fd739f2f5694dd0a48d6d9494fbf3c50ac156171
Valeriia Bokher <valeriiabokher@gmaol.com>
2023-02-21 19:07:13 +0200

    file:new files

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+		this.min = 0;
+		this.max = 0;
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    }
+	guess() {
+		this.mid = Math.round((this.max - this.min) / 2) + this.min;
+		return this.mid;
+	}
 
-    guess() {
+	lower() {
+		this.max = this.mid;
+	}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = this.mid;
+	}
 }
 
 module.exports = GuessingGame;
